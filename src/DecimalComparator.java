bede7fb1806f7b9a7e8381447dbbdc6b81032ef2
Boldeusz <mnpower@poczta.onet.pl>
2021-01-17 18:26:38 +0100

    update

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,2 +1,9 @@
 public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double one, double two){
+        int a = (int)(one * 1000);
+        int b = (int)(two * 1000);
+        if (a == b){
+            return true;
+        }return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(2.301, 2.301));
+
     }
 }
